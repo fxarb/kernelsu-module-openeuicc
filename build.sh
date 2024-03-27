af1610a5da6ac33ea60cf9faad8881f73c94fc6c
fxarb <52603276+fxarb@users.noreply.github.com>
2024-03-27 10:59:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd OpenEUICC
-./gradlew build -x test
+./gradlew :app:assembleRelease
 cd .. && mkdir -p system/system_ext/priv-app/OpenEUICC
-cp OpenEUICC/app/build/outputs/apk/debug/app-debug.apk system/system_ext/priv-app/OpenEUICC/OpenEUICC.apk
+cp OpenEUICC/app/build/outputs/apk/release/app-release.apk system/system_ext/priv-app/OpenEUICC/OpenEUICC.apk
 mkdir -p system/system_ext/etc/permissions/
 cp OpenEUICC/privapp_whitelist_im.angry.openeuicc.xml system/system_ext/etc/permissions/privapp_whitelist_im.angry.openeuicc.xml
 zip -FSr module.zip customize.sh module.prop system/ uninstall.sh
